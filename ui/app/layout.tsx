08ebe3f15213b38a54af5544cb2996e7dfe883d8
Brian Mego <brianmego@gmail.com>
2024-12-29 22:33:15 -0600

    Messing with theme

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Providers } from "./providers";
 import { Inter } from "next/font/google";
-import "./ui/globals.css";
+import "@/app/ui/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- ui/tailwind.config.ts
+++ ui/tailwind.config.ts
@@ -1,63 +1,17 @@
 import {nextui} from '@nextui-org/theme';
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(listbox|divider|button|code|input).js"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   darkMode: "class",
-    plugins: [nextui(
-        {
-      themes: {
-        light: {
-          colors: {
-            background: "#FFFFFF", // or DEFAULT
-            foreground: "#11181C", // or 50 to 900 DEFAULT
-            primary: {
-              //... 50 to 900
-              foreground: "#FFFFFF",
-              DEFAULT: "#006FEE",
-            },
-            // ... rest of the colors
-          },
-        },
-        dark: {
-          colors: {
-            background: "#000000", // or DEFAULT
-            foreground: "#ECEDEE", // or 50 to 900 DEFAULT
-            primary: {
-              //... 50 to 900
-              foreground: "#FFFFFF",
-              DEFAULT: "#006FEE",
-            },
-          },
-          // ... rest of the colors
-        },
-        mytheme: {
-          // custom theme
-          extend: "dark",
-          colors: {
-            primary: {
-              DEFAULT: "#BEF264",
-              foreground: "#000000",
-            },
-            focus: "#BEF264",
-          },
-        },
-      },
-    }
-    )],
+    plugins: [nextui()],
 };
 export default config;
